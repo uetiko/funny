b2b3ce3fd74cbf2091ef0b9cbaf56a5f96bc7354
Angel Barrientos <abarrietos@nyxtechnology.com>
2016-06-29 17:54:13 -0500

    Solo comentarios

--- closures/fizzbuzz.py
+++ closures/fizzbuzz.py
@@ -2,10 +2,18 @@
 Un divertido fizzbuzz
 '''
 def fizz(fn, number):
+    '''
+    Función que detona el closure dentro de
+    un for que recibe un rango
+    '''
     for x in range(number):
         fn(x)
 
 def buzz(x):
+    '''
+    La funcion que hace la magia pokemon del
+    fizzbuzz
+    '''
     if x % 3 == 0 and x % 5 == 0:
         print 'FizzBuzz'
     elif x % 3 == 0:
